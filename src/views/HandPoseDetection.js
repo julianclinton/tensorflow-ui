7dd72931ebbeeb3a771495fb8d5261a41bf98878
Julian Clinton <julian@Julians-Mac-mini.local>
2022-11-05 14:40:02 +0000

    Add lines to hand drawing

--- src/views/HandPoseDetection.js
+++ src/views/HandPoseDetection.js
@@ -5,27 +5,39 @@ import Row from 'react-bootstrap/Row'
 import RangeSlider from 'react-bootstrap-range-slider'
 import Loading from '../components/Loading'
 import VideoPanel from '../components/VideoPanel'
-import { SKELETON_COLOR, drawPoint, drawPath } from '../util/drawing'
+import { drawPoint, drawPath } from '../util/drawing'
 import '@tensorflow/tfjs-backend-webgl'
 const handdetection = require('@tensorflow-models/hand-pose-detection')
 
-const drawKeypoints = (hand, scale, ctx) => {
-  const keypointsArray = hand.keypoints
+const FINGER_LOOKUP_INDICES = {
+  thumb: [0, 1, 2, 3, 4],
+  indexFinger: [0, 5, 6, 7, 8],
+  middleFinger: [0, 9, 10, 11, 12],
+  ringFinger: [0, 13, 14, 15, 16],
+  pinky: [0, 17, 18, 19, 20],
+} // for rendering each finger as a polyline
 
-  for (let i = 0; i < keypointsArray.length; i++) {
-    const y = keypointsArray[i].x * scale;
-    const x = keypointsArray[i].y * scale;
-    drawPoint(ctx, x - 2, y - 2, 3, SKELETON_COLOR);
+const LEFT_HAND_COLOUR = 'Red'
+const RIGHT_HAND_COLOUR = 'Blue'
+
+const drawKeypoints = (hand, scale, ctx) => {
+  const handedness = hand.handedness
+  const keypoints = hand.keypoints
+  ctx.fillStyle = handedness === 'Left' ? LEFT_HAND_COLOUR : RIGHT_HAND_COLOUR
+  ctx.strokeStyle = 'White'
+
+  for (let i = 0; i < keypoints.length; i++) {
+    const y = keypoints[i].x * scale;
+    const x = keypoints[i].y * scale;
+    drawPoint(ctx, x - 2, y - 2, 3, ctx.fillStyle);
   }
 
-  /*
-  const fingers = Object.keys(handdetection.FINGER_LOOKUP_INDICES);
+  const fingers = Object.keys(FINGER_LOOKUP_INDICES);
   for (let i = 0; i < fingers.length; i++) {
     const finger = fingers[i];
-    const points = handdetection.FINGER_LOOKUP_INDICES[finger].map(idx => keypoints[idx]);
-    drawPath(ctx, points, SKELETON_COLOR, scale, false);
+    const points = FINGER_LOOKUP_INDICES[finger].map(idx => [keypoints[idx].x, keypoints[idx].y]);
+    drawPath(ctx, points, ctx.fillStyle, scale, false);
   }
-  */
 }
 
 export const HandPoseDetection = (props) => {
