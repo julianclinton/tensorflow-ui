34d56d9e3a952c75d137aa07c53420346fd07ec0
Julian Clinton <julian@Julians-Mac-mini.local>
2024-11-11 08:51:57 +0000

    test: fix the App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
 test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/Home/i)
-  expect(linkElement).toBeInTheDocument()
+  render(<BrowserRouter><App /></BrowserRouter>)
+  const element = screen.getByText(/TensorFlow Play/i)
+  expect(element).toBeInTheDocument()
 })
